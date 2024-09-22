29f89fe2c73f6ec07afd2cd0141c81dd1a891d17
McNaBry <mcnabry123@gmail.com>
2024-09-22 17:46:07 +0800

    Add error toast message for login and registration pages

--- frontend/src/app/account/login.component.ts
+++ frontend/src/app/account/login.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SelectButtonChangeEvent, SelectButtonModule } from 'primeng/selectbutton';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
-import { DividerModule } from 'primeng/divider';
-import { Router } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule, InputTextModule, ButtonModule, SelectButtonModule, PasswordModule, DividerModule],
+    imports: [FormsModule, InputTextModule, ButtonModule, SelectButtonModule, PasswordModule, ToastModule],
+    providers: [MessageService],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
 })
 export class LoginComponent {
+    constructor(private router: Router, private messageService: MessageService) {}
+
     user = {
         username: '',
         password: '',
@@ -28,18 +32,20 @@ export class LoginComponent {
 
     isProcessingLogin = false;
 
-    constructor(private router: Router) {}
-
     onOptionChange(event: SelectButtonChangeEvent) {
         if (event.value == 'register') {
             this.router.navigate(['/account/register']);
         }
     }
 
+    showError() {
+        this.messageService.add({ severity: 'error', summary: 'Log In Error', detail: 'Missing Details' })
+    }
+
     onSubmit() {
         if (this.user.username && this.user.password) {
             this.isProcessingLogin = true;
-
+            this.showError();
             // Simulate API request
             setTimeout(() => {
                 this.isProcessingLogin = false;
--- frontend/src/app/account/register.component.ts
+++ frontend/src/app/account/register.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SelectButtonChangeEvent, SelectButtonModule } from 'primeng/selectbutton';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
-import { Router } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [FormsModule, InputTextModule, ButtonModule, SelectButtonModule, PasswordModule, DividerModule],
+    imports: [FormsModule, InputTextModule, ButtonModule, SelectButtonModule, PasswordModule, DividerModule, ToastModule],
+    providers: [MessageService],
     templateUrl: './register.component.html',
     styleUrl: './login.component.css',
 })
 export class RegisterComponent {
+    constructor(private router: Router, private messageService: MessageService) {}
+
     user = {
         username: '',
         email: '',
@@ -30,17 +35,20 @@ export class RegisterComponent {
 
     isProcessingRegistration = false;
 
-    constructor(private router: Router) {}
-
     onOptionChange(event: SelectButtonChangeEvent) {
         if (event.value == 'login') {
             this.router.navigate(['/account/login']);
         }
     }
 
+    showError() {
+        this.messageService.add({ severity: 'error', summary: 'Registration Error', detail: 'Missing Details' })
+    }
+
     onSubmit() {
         if (this.user.username && this.user.password) {
             this.isProcessingRegistration = true;
+            this.showError();
             setTimeout(() => {
                 this.isProcessingRegistration = false;
                 console.log('Form Submitted', this.user);
