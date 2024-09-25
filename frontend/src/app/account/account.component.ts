c7dd4be06fc0184ad76c074d42e4b9cea573f6eb
McNaBry <mcnabry123@gmail.com>
2024-09-25 14:43:00 +0800

    Redirect account route to login

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
         path: '',
         component: LayoutComponent,
         children: [
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
             { path: 'login', component: LoginComponent },
             { path: 'register', component: RegisterComponent },
         ],
