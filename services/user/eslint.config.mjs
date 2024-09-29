65052bf202faaa816a6260de9924cd0edc9ff2a6
Samuel Lim <samuelim01@gmail.com>
2024-09-29 17:17:30 +0800

    Add user service prettier

--- services/user/eslint.config.mjs
+++ services/user/eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config({
     files: ['**/*.ts'],
@@ -9,6 +10,18 @@ export default tseslint.config({
         eslint.configs.recommended,
         ...tseslint.configs.strict,
         ...tseslint.configs.stylistic,
+        eslintPluginPrettierRecommended,
     ],
-    rules: {},
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        "@typescript-eslint/no-extraneous-class": "off",
+
+        // https://stackoverflow.com/questions/68816664/get-rid-of-error-delete-eslint-prettier-prettier-and-allow-use-double
+        'prettier/prettier': [
+            'error',
+            {
+                'endOfLine': 'auto',
+            }
+        ]
+    },
 });
