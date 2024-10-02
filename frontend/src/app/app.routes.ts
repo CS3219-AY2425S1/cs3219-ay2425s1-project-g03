0cd1609b453a378f42df6b0b5e91024281a71a1e
limcaaarl <carljovi22@gmail.com>
2024-10-02 23:45:22 +0800

    Set up layout for question box and editor

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { QuestionsComponent } from './questions/questions.component';
 import { CollaborationComponent } from './collaboration/collaboration.component';
-import { MatchingComponent } from './matching/matching.component';
-import { AuthGuardService } from '../_services/auth.guard.service';
-import { CollabGuardService } from '../_services/collab.guard.service';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 
@@ -26,5 +23,9 @@ export const routes: Routes = [
         path: 'matching',
         component: MatchingComponent,
         canActivate: [AuthGuardService],
+    },
+    {
+        path: 'start',
+        component: CollaborationComponent,
     },
 ];
--- frontend/src/app/collaboration/question-box/question-box.component.ts
+++ frontend/src/app/collaboration/question-box/question-box.component.ts
@@ -3,55 +3,35 @@ import { Question } from '../../questions/question.model';
 import { QuestionService } from '../../../_services/question.service';
 import { ChipModule } from 'primeng/chip';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
-import { DifficultyLevels } from '../../questions/difficulty-levels.enum';
-import { MessageService } from 'primeng/api';
-import { CollabService } from '../../../_services/collab.service';
-import { RoomService } from '../room.service';
 
 @Component({
     selector: 'app-question-box',
     standalone: true,
     imports: [ChipModule, ScrollPanelModule],
-    providers: [QuestionService, MessageService],
+    providers: [QuestionService],
     templateUrl: './question-box.component.html',
     styleUrl: './question-box.component.css',
 })
 export class QuestionBoxComponent implements OnInit {
-    question = {} as Question;
-    difficultyLevels = DifficultyLevels;
-    roomId!: string;
+    question!: Question;
 
-    constructor(
-        private collabService: CollabService,
-        private messageService: MessageService,
-        private roomService: RoomService,
-    ) {}
+    questionId = 1;
+
+    constructor(private questionService: QuestionService) {}
 
     ngOnInit() {
-        this.initRoomId();
-        this.initQuestion();
+        this.setQuestion();
     }
 
-    initRoomId() {
-        this.roomService.getRoomId().subscribe(id => {
-            this.roomId = id!;
-        });
-    }
-
-    initQuestion() {
-        this.collabService.getRoomDetails(this.roomId).subscribe({
-            next: response => {
-                this.question = response.data.question;
-            },
-            error: () => {
-                this.question = {} as Question;
-                this.messageService.add({
-                    severity: 'error',
-                    summary: 'Error',
-                    detail: 'Failed to retrieve room details',
-                    life: 3000,
-                });
-            },
-        });
+    setQuestion() {
+        // dummy question
+        this.question = {
+            id: 1,
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            difficulty: 'Hard',
+            title: 'Lorem Ipsum',
+            topics: ['Topic 1', 'Topic 2', 'Topic 3', 'Topic 4'],
+        };
     }
 }
