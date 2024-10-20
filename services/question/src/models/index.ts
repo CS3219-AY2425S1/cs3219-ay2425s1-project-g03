6992cc6ac50ab8da508fd6b74d7205ab9a382854
KhoonSun47 <justforwks@gmail.com>
2024-10-20 20:20:59 +0800

    Update configurations on MongoDB - changing URI

--- services/question/src/models/index.ts
+++ services/question/src/models/index.ts
@@ -3,7 +3,18 @@ import { IQuestion, Question } from './questionModel';
 import config from '../config';
 
 export async function connectToDB() {
-    await mongoose.connect(config.DB_URI, {
+    const mongoURI = process.env.QUESTION_DB_CLOUD_URI || "mongodb://localhost:27017/question-";
+
+    console.log('MongoDB URI:', mongoURI);
+    console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD);
+
+    if (!mongoURI) {
+        throw new Error('MongoDB URI not specified');
+    } else if (!process.env.DB_USERNAME || !process.env.DB_PASSWORD) {
+        throw Error('MongoDB credentials not specified');
+    }
+
+    await mongoose.connect(mongoURI, {
         authSource: 'admin',
         user: config.DB_USERNAME,
         pass: config.DB_PASSWORD,
