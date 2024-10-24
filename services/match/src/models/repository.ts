3e0a0e542e65b3f1b574b949b9b44a730152d0b8
Samuel Lim <samuelim01@gmail.com>
2024-10-24 21:49:19 +0800

    Match: Centralize env variables

--- services/match/src/models/repository.ts
+++ services/match/src/models/repository.ts
@@ -1,24 +1,23 @@
 import mongoose, { Types } from 'mongoose';
 import { Difficulty, MatchRequestModel } from './matchRequestModel';
 import { oneMinuteAgo } from '../utils/date';
+import config from '../config';
 
 type IdType = string | Types.ObjectId;
 
 export async function connectToDB() {
-    const mongoURI = process.env.NODE_ENV === 'production' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+    const mongoURI = config.NODE_ENV === 'production' ? config.DB_CLOUD_URI : config.DB_LOCAL_URI;
 
     console.log('MongoDB URI:', mongoURI);
 
     if (!mongoURI) {
         throw new Error('MongoDB URI not specified');
-    } else if (!process.env.DB_USERNAME || !process.env.DB_PASSWORD) {
-        throw Error('MongoDB credentials not specified');
     }
 
     await mongoose.connect(mongoURI, {
         authSource: 'admin',
-        user: process.env.DB_USERNAME,
-        pass: process.env.DB_PASSWORD,
+        user: config.DB_USERNAME,
+        pass: config.DB_PASSWORD,
     });
 }
 
