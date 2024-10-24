89fde3e460d55ad0146d83f1ee5e24fc6d0aee8b
Samuel Lim <samuelim01@gmail.com>
2024-10-24 21:18:16 +0800

    Question: Centralize env variables

---
+++ services/question/src/config.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+const envSchema = z
+    .object({
+        DB_USERNAME: z.string().trim().min(1),
+        DB_PASSWORD: z.string().trim().min(1),
+        DB_CLOUD_URI: z.string().trim().optional(),
+        DB_LOCAL_URI: z.string().trim().optional(),
+        NODE_ENV: z.enum(['development', 'production']).default('development'),
+        CORS_ORIGIN: z.union([z.string().url(), z.literal('*')]).default('*'),
+        PORT: z.coerce.number().min(1024).default(8081),
+    })
+    .refine(
+        data => {
+            const isUrl = z.string().url();
+
+            if (data.NODE_ENV === 'production') {
+                return isUrl.safeParse(data.DB_CLOUD_URI).success;
+            } else {
+                return isUrl.safeParse(data.DB_LOCAL_URI).success;
+            }
+        },
+        {
+            message: 'Database URI must be provided',
+            path: ['DB_CLOUD_URI', 'DB_LOCAL_URI'],
+        },
+    );
+
+const result = envSchema.safeParse(process.env);
+if (!result.success) {
+    console.error('There is an error with the environment variables:', result.error.issues);
+    process.exit(1);
+}
+const config = result.data;
+
+export default config;
--- services/question/src/models/index.ts
+++ services/question/src/models/index.ts
@@ -1,21 +1,22 @@
 import mongoose from 'mongoose';
 import { IQuestion, Question } from './questionModel';
+import config from '../config';
 
 export async function connectToDB() {
-    const mongoURI = process.env.NODE_ENV === 'production' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+    const mongoURI = config.NODE_ENV === 'production' ? config.DB_CLOUD_URI : config.DB_LOCAL_URI;
 
     console.log('MongoDB URI:', mongoURI);
 
     if (!mongoURI) {
         throw new Error('MongoDB URI not specified');
-    } else if (!process.env.DB_USERNAME || !process.env.DB_PASSWORD) {
+    } else if (!config.DB_USERNAME || !config.DB_PASSWORD) {
         throw Error('MongoDB credentials not specified');
     }
 
     await mongoose.connect(mongoURI, {
         authSource: 'admin',
-        user: process.env.DB_USERNAME,
-        pass: process.env.DB_PASSWORD,
+        user: config.DB_USERNAME,
+        pass: config.DB_PASSWORD,
     });
 }
 
