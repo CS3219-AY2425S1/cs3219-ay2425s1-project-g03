ed95e7f1aa395ea3c3c35d8692a357b5d49c580f
KhoonSun47 <justforwks@gmail.com>
2024-10-29 20:27:09 +0800

    Minor Fix Based On Comments: - README.md: Update port from 8087 to 8084Z - roomController.ts: Update the helper methods - webSocketService.ts: Update the helper methods - helper.ts: Seperate the helper methods for HTTPS and WebSocket

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -20,7 +20,6 @@ export const routes: Routes = [
     {
         path: 'collab',
         component: CollaborationComponent,
-        // canActivate: [AuthGuardService],
         canActivate: [AuthGuardService, CollabGuardService],
     },
     {
