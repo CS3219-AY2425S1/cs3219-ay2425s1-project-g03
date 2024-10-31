195af4b6b2f1a2db75c1d6ef32b653edcb93c1e3
KhoonSun47 <justforwks@gmail.com>
2024-10-31 20:54:13 +0800

    Backbone of Chat Service: - Basic HTML and CSS for Chat - Basic Functionality for Chat

--- frontend/src/app/collaboration/chat-box/chat-box.component.ts
+++ frontend/src/app/collaboration/chat-box/chat-box.component.ts
@@ -1,24 +1,60 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as Y from 'yjs';
+import { AuthenticationService } from '../../../_services/authentication.service';
+import { WebsocketProvider } from 'y-websocket';
+import { NgForOf } from '@angular/common';
 
 @Component({
-    selector: 'app-chat-box',
-    standalone: true,
-    imports: [],
-    templateUrl: './chat-box.component.html',
-    styleUrl: './chat-box.component.css',
+  selector: 'app-chat-box',
+  standalone: true,
+  imports: [NgForOf],
+  templateUrl: './chat-box.component.html',
+  styleUrls: ['./chat-box.component.css'],
 })
-export class ChatBoxComponent implements OnInit {
-    @Input() ydoc!: Y.Doc;
+export class ChatBoxComponent implements OnInit, AfterViewInit {
+  @Input() ydoc!: Y.Doc;
+  @Input() wsProvider!: WebsocketProvider;
+  @Input() roomId!: string;
 
-    // TODO change this type accordingly
-    ychat = new Y.Array<string>();
+  @ViewChild('chatInput') chatInput!: ElementRef;
 
-    ngOnInit() {
-        this.initDoc();
+  messages: string[] = [];
+  yChatArray!: Y.Array<string>;
+  userId!: string;
+
+  constructor(private authService: AuthenticationService) {}
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.getUserId();
+    this.initYdoc();
+    this.initArrayListener();
+  }
+
+  getUserId() {
+    this.userId = this.authService.userValue?.id || 'Guest';
+  }
+
+  initYdoc() {
+    this.yChatArray = this.ydoc.getArray<string>('chatMessages');
+  }
+
+  initArrayListener() {
+    this.yChatArray.observe(() => {
+      this.messages = this.yChatArray.toArray();
+    });
+  }
+
+  sendMessage() {
+    const message = this.chatInput.nativeElement.value.trim();
+    if (message) {
+      const formattedMessage = `${this.userId}: ${message}`;
+
+      this.yChatArray.push([formattedMessage]);
+
+      this.chatInput.nativeElement.value = '';
     }
-
-    initDoc() {
-        this.ychat = this.ydoc.getArray('chat');
-    }
+  }
 }
