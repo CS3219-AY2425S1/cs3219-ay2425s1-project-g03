bd643999e1dd4f5bbd6ed1011b515c93169f1d60
Samuel Lim <samuelim01@gmail.com>
2024-11-14 09:31:35 +0800

    Simplify history tests

--- services/history/tests/fixtures.ts
+++ services/history/tests/fixtures.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.sample' });
-
 import jwt from 'jsonwebtoken';
 import { Types } from 'mongoose';
 import { Difficulty, Question, User } from '../src/models/historyModel';
